a2b308a6a9b34e76c4edd3f0c0977bf30d297612
Rajat saxena <saxenarajat499@gmail.com>
2025-09-08 14:21:20 +0530

    anans

--- backend/main/urls.py
+++ backend/main/urls.py
@@ -4,7 +4,7 @@ from django.urls import path,include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("django/basics/", include("Basic.urls")),
+    path("django/", include("Basic.urls")),
     path("django/drain-water-quality/", include("dashboard.urls")),
 
 ]
