170d4026cdb6ca8ec85d05aaaa100c86f710acc0
Rajat saxena <saxenarajat499@gmail.com>
2026-02-05 12:17:46 +0530

    fixing

--- frontend/app/dss/rwm/wwt/stp_priority/admin/page.tsx
+++ frontend/app/dss/rwm/wwt/stp_priority/admin/page.tsx
@@ -11,7 +11,7 @@ import { useLocation } from "@/contexts/stp_priority/admin/LocationContext";
 import { useCategory } from "@/contexts/stp_priority/admin/CategoryContext";
 import MapView from "@/app/dss/rwm/wwt/stp_priority/admin/components/openlayer";
 import { useMap } from "@/contexts/stp_priority/admin/MapContext";
-import { CategorySlider } from "./components/weight_slider";
+import CategorySlider from "./components/weight_slider";
 import { toast } from "react-toastify";
 import DataTable from "react-data-table-component";
 import { Village_columns } from "@/interface/table";
