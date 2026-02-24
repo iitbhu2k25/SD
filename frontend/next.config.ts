961b6272f552df86385086e9356847dee1da8393
Rajat saxena <saxenarajat499@gmail.com>
2026-02-24 16:58:51 +0530

    new update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -14,22 +14,6 @@ const nextConfig: NextConfig = {
   },
   async rewrites() {
     return [
-      {
-        source: '/api/:path*',
-        destination: "http://fast_backend:7000/api/:path*"
-      },
-      {
-        source: '/access_token/:path*',
-        destination: "http://fast_backend:7000/access_token/:path*"
-      },
-      {
-        source: "/django/:path*",
-        destination: "http://backend:9000/django/:path*",
-      },
-      {
-        source: "/fastm/:path*",
-        destination: "http://fast_m:6500/:path*",
-      },
     ]
   },
 }
